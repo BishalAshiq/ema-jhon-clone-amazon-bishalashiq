95e87639511b99b9c184d351d3913e7fe43327f0
BishalAshiq <bishaltheboss20@gmail.com>
2021-10-03 16:54:07 +0600

    updated inventory

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -39,7 +39,18 @@ const Shop = () => {
     }, [products]);
 
     const handleAddToCart= (product) =>{
-        const newCart =[...cart, product]
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists){
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, product];
+        }
+        else{
+            product.quantity= 1;
+            newCart = [...cart, product];
+        }
+        console.log(newCart);
         setCart(newCart);
         //save to local storage (for now)
         addToDb(product.key)
