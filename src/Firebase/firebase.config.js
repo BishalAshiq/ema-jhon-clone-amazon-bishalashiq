e0c03ad049bf4864fa06fcd57565e146f2caf461
BishalAshiq <bishaltheboss20@gmail.com>
2021-10-10 01:19:22 +0600

    auth

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC75CZqYCm0V29q7jIUx8jJMNwrIsVG-PM",
+    authDomain: "ema-jhon-simple-a886b.firebaseapp.com",
+    projectId: "ema-jhon-simple-a886b",
+    storageBucket: "ema-jhon-simple-a886b.appspot.com",
+    messagingSenderId: "54399362124",
+    appId: "1:54399362124:web:d19ec01488905a40a780e9"
+  };
+
+  export default firebaseConfig;
